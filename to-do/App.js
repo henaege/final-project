746da0cf69bd8a41145c71538b50cf0e9e0cfa5c
YINGRONG ZHAO <lionzhao0820@gmail.com>
2017-07-24 13:33:52 -0400

    Added basic components

--- to-do/App.js
+++ to-do/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Login from './components/Login';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
         <View style={styles.buttons}>
+          
           <Button onPress={()=>{}} title="Sign Up" />
           <Button onPress={()=>{}} title="Sign In" />
 
---
+++ to-do/components/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+const Input = ({ lable, value, onChangeText })=>{
+	const { inputStyle, lableStyle, containerStyle } = styles;
+	return(
+		<View style={containerStyle}>
+			<Text style={lableStyle}>{lable}</Text>
+			<TextInput 
+				style={inputStyle}
+				value={value}
+				onChangeText={onChangeText}
+			/>
+		</View>
+	)
+}
+const styles={
+	inputStyle: {
+		color:'#000',
+		paddingRight: 5,
+		paddingLeft: 5,
+		fontSize: 18,
+		lineHeight: 23,
+		flex: 3,
+	},
+	lableStyle: {
+		fontSize:18,
+		paddingLeft: 20,
+		flex: 1,
+	},
+	containerStyle: {
+		height: 40,
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+	}
+}
+export { Input };
