741b4d9703cf0b77236f5baa754933c1a7abaac1
henaege <henaege@gmail.com>
2017-07-24 11:09:20 -0400

    DNU

---
+++ to-do/components/Start.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Button, Text } from 'native-base';
+export default class Start extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>
+          <Text>
+            Button
+          </Text>
+        </Button>
+      </Container>
+    );
+  }
+}
